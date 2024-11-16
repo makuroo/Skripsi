e0b3b5ed71230390ee232e49b681e08d09d5769e
makuroo <richardhuang1705@gmail.com>
2024-11-16 17:42:22 +0700

    minor addition

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -4,12 +4,22 @@ using System.Collections.Generic;
 using Manager;
 using UnityEngine;
 using AYellowpaper.SerializedCollections;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private SerializedDictionary<ScoresEnum, ScoreUI> _scoreUIDictionary = new();
+    [Header("LosePanel")]
+    [Space(5)]
     [SerializeField] private GameObject _gameOverPanel;
+    [SerializeField] private TMP_Text _ratingScoreLose;
+    [SerializeField] private TMP_Text _incomeScoreLose;
+    
+    [Header("WinPanel")]
+    [Space(5)]
     [SerializeField] private GameObject _playerWinPanel;
+    [SerializeField] private TMP_Text _ratingScoreWin;
+    [SerializeField] private TMP_Text _incomeScoreWin;
     public static Action<ScoresEnum, int> UpdateScoreUI;
     public static Action PlayerLose;
     public static Action PlayerWin;
@@ -39,12 +49,16 @@ public class UIManager : MonoBehaviour
     private void OnPlayerLose()
     {
         Time.timeScale = 0;
+        _ratingScoreLose.text = GameManager.Instance.RatingScore.ToString();
+        _incomeScoreLose.text = GameManager.Instance.IncomeScore.ToString();
         _gameOverPanel.SetActive(true);
     }
 
     private void OnPlayerWin()
     {
         Time.timeScale = 0;
+        _ratingScoreWin.text = GameManager.Instance.RatingScore.ToString();
+        _incomeScoreWin.text = GameManager.Instance.IncomeScore.ToString();
         _playerWinPanel.SetActive(true);
     }
 }
